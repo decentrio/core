488184fffadaa89849b03faee7cb956b64cc127c
Hieu Vu <gundamaster5@gmail.com>
2024-06-13 03:33:30 +0700

    vendor sdk

---
+++ cosmos-sdk/store/store.go
@@ -0,0 +1,22 @@
+package store
+
+import (
+	dbm "github.com/cometbft/cometbft-db"
+	"github.com/cometbft/cometbft/libs/log"
+
+	"github.com/cosmos/cosmos-sdk/store/cache"
+	"github.com/cosmos/cosmos-sdk/store/rootmulti"
+	"github.com/cosmos/cosmos-sdk/store/types"
+)
+
+func NewCommitMultiStore(db dbm.DB) types.CommitMultiStore {
+	return rootmulti.NewStore(db, log.NewNopLogger())
+}
+
+func NewCommitMultiStoreWithLogger(db dbm.DB, logger log.Logger) types.CommitMultiStore {
+	return rootmulti.NewStore(db, logger)
+}
+
+func NewCommitKVStoreCacheManager() types.MultiStorePersistentCache {
+	return cache.NewCommitKVStoreCacheManager(cache.DefaultCommitKVStoreCacheSize)
+}
